98060cc2939c852d6cb7f7f4cc7c7543dc3546b8
Nicholai <kranoscorp@gmail.com>
2015-02-18 15:05:47 -0500

    fixed nav bar, normalized aesthetics with home website, created a better splash.

--- index.php
+++ index.php
@@ -3,82 +3,97 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>CS418 Project 1</title>
-
-    <!-- My Icon -->
-    <link rel="shortcut icon" href="img/N.ico" type="image/x-icon" />
-
-    <!--foundation-->
+    <title>Nicholai Goodall // Portfolio</title>
+      
     <link rel="stylesheet" href="css/foundation.css" />
     <script src="js/vendor/modernizr.js"></script>
+      
+    <link rel="shortcut icon" href="img/N.ico" type="image/x-icon" />
 
-    <!--Font Awesome-->
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-
-    <!--Google Font: Lobster-->
+    <!-- Custom styles for this template -->
+    <link href="css/default.css" rel="stylesheet">
+      
+    <!-- Google Font: Lobster -->
     <link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
-
-    <!--Google Font: Ubuntu-->
+      
+    <!-- Google Font: Ubuntu -->
     <link href='http://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
-
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
   </head>
   <body>
-
+      
     <!--
-    ============================================================================
-                                    Navbar
-    ============================================================================
+    =====================================================================================================
+                                                  Nav Bar
+    =====================================================================================================
     -->
-    <div class="row">
-      <div class="large-12 columns">
-        <div class="navbar">
-            <div class="panel">
-              <div class="row">
-                <div class="large-1 medium-2 small-3 columns">
-                  <div class="callout panel"></div>
-                </div>
+    <div class="fixed">
+        <nav class="top-bar" data-topbar role="navigation">
+          <ul class="title-area">
+            <li class="name">
+              <h1><a href="http://www.nichol.ai" id="name">N</a></h1>
+            </li>
+             <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
+            <li class="toggle-topbar"><a href="#"><span id="connect">connect</span></a></li>
+          </ul>
 
-              </div>
-            </div>
-        </div>
+          <section class="top-bar-section">
+            <!-- Right Nav Section -->
+            <ul class="right">
+              <li><a href="http://github.com/catlover91" title="Github"><i class="fa fa-2x fa-github"></i></a></li>
+              <li><a href="http://twitter.com/me0wmixgg" title="Twitter"><i class="fa fa-2x fa-twitter"></i></a></li>
+              <li>
+                <a href="http://about.me/kranoscorp" title="About.me">
+                  <span class="fa-stack fa-lg">
+                    <i class="fa fa-circle fa-stack-2x"></i>
+                    <strong class="fa-stack-1x about-me-text">Me</strong>
+                  </span>
+                </a>
+              </li>
+            </ul>
+
+            <u1 class="left">
+              <!-- Empty right now -->
+            </u1>
+          </section>
+        </nav>
+    </div>
+    
+      
+    <!--
+    =====================================================================================================
+                                                 Splash
+    =====================================================================================================
+    -->
+    <div class="parallax-background">
+      <div class="intro-text">
+        <h1>Q & A</h1>
+        <h2>Nicholai Goodall</h2>
       </div>
     </div>
-
-    <!--
-    ============================================================================
-                                    Splash
-    ============================================================================
-    -->
-
-
-    <div class="row">
-      <div class="large-12 columns">
-      	<div class="panel">
-	        <h3>Welcome to my question and answer site!</h3>
-	        <p>This website contains my project for my web development class!</p>
-	        <div class="row">
-	        	<div class="large-4 medium-4 columns">
-	    		<p><a href="http://www.nichol.ai">Nichol.ai</a><br/>This is designed by me, Nicholai Goodall. You can find my home web page here. </p>
-	    	</div>
-	        	<div class="large-4 medium-4 columns">
-	        		<p><a href="http://github.com/catlover91/ODUCS418">Github Repo</a><br/>You can find the code for this website here.</p>
-	        	</div>
-            </div>
-      	</div>
-      </div>
-    </div>
+      
+    
+    
+    
 
     <!--
     ============================================================================
                                     Main Block
     ============================================================================
     -->
+    <div class="parallax-content" id="adjust-padding">
+        <p><a href="http://github.com/catlover91/ODUCS418">Github Repo</a><br/>You can find the code for this website here.</p>
+        <div class="row">
+            
+            <script type="text/javascript" src="server.php"></script>
 
-    <div class="row">
-        
-        <script type="text/javascript" src="server.php"></script>
-        
+        </div>
     </div>
-
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
   </body>
 </html>
