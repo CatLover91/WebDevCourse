1ff174730b5bcfd4a37b366f977e4aed9d6b1cd3
Nicholai Goodall <Kranoscorp@gmail.com>
2015-03-16 17:06:32 -0400

    lightened question

--- resources/views/questionlite.blade.php
+++ resources/views/questionlite.blade.php
@@ -1,41 +1,34 @@
-@extends('app')
-
-@section('content')
-
-@foreach($questions as $question)
-    <div class="row-fluid color-box">
-        <div class="col-xs-2">
-            @if(Auth::guest() || Auth::user()->id === $question.asker_id)
-                <label>{{ $question.Vote }}</label>
-            @else
-                <span class="fa-stack fa-lg">
-                    <i class="fa fa-square fa-stack-2x"></i>
-                    <i class="fa fa-chevron-up fa-stck-1x"></i>
-                </span>
-                <label>{{ $question.Vote }}</label>
-                <span class="fa-stack fa-lg">
-                    <i class="fa fa-square fa-stack-2x"></i>
-                    <i class="fa fa-chevron-down fa-stack-1x"></i>
-                </span>
-            @endif
-        </div>
-        <div class="col-xs-8">
-            <h2>{{ $question.Title }}</h2>
-            <p>{{ $question.ContentLite }}</p>
-        </div>
-        <div class="col-xs-2">
-            @if($Author.hasProfile())
-                <!--Display Profile Photo-->
-            @else
-                <span class="fa-stack fa-lg">
-                    <i class="fa fa-square fa-stack-x2"></i>
-                    <i class="fa fa-user fa-stack-1x"></i>
-                </span>
-            @endif
-        </div>
-        <div class="col-xs-12">
-            <label>{{ $question.Author.Name }}</label>
-        </div>
+<div class="row-fluid color-box">
+    <div class="col-xs-2">
+        @if(Auth::guest() || Auth::user()->id === $question.asker_id)
+            <label>{{ $question.Vote }}</label>
+        @else
+            <span class="fa-stack fa-lg">
+                <i class="fa fa-square fa-stack-2x"></i>
+                <i class="fa fa-chevron-up fa-stck-1x"></i>
+            </span>
+            <label>{{ $question.Vote }}</label>
+            <span class="fa-stack fa-lg">
+                <i class="fa fa-square fa-stack-2x"></i>
+                <i class="fa fa-chevron-down fa-stack-1x"></i>
+            </span>
+        @endif
     </div>
-@endforeach
-@endsection
+    <div class="col-xs-8">
+        <h2>{{ $question.Title }}</h2>
+        <p>{{ $question.ContentLite }}</p>
+    </div>
+    <div class="col-xs-2">
+        @if($Author.hasProfile())
+            <!--Display Profile Photo-->
+        @else
+            <span class="fa-stack fa-lg">
+                <i class="fa fa-square fa-stack-x2"></i>
+                <i class="fa fa-user fa-stack-1x"></i>
+            </span>
+        @endif
+    </div>
+    <div class="col-xs-12">
+        <label>{{ $question.Author.Name }}</label>
+    </div>
+</div>
