09bd8254f7ea24c5b0b7ded57492e73b0f1b5e95
Nicholai Goodall <Kranoscorp@gmail.com>
2015-03-16 17:28:39 -0400

    Update questionlite.blade.php

--- resources/views/questionlite.blade.php
+++ resources/views/questionlite.blade.php
@@ -1,13 +1,13 @@
 <div class="row-fluid color-box">
     <div class="col-xs-2">
         @if(Auth::guest() || Auth::user()->id === $question.asker_id)
-            <label>{{ $question.Vote }}</label>
+            <label>{{ $question.vote }}</label>
         @else
             <span class="fa-stack fa-lg">
                 <i class="fa fa-square fa-stack-2x"></i>
                 <i class="fa fa-chevron-up fa-stck-1x"></i>
             </span>
-            <label>{{ $question.Vote }}</label>
+            <label>{{ $question.vote }}</label>
             <span class="fa-stack fa-lg">
                 <i class="fa fa-square fa-stack-2x"></i>
                 <i class="fa fa-chevron-down fa-stack-1x"></i>
@@ -15,20 +15,10 @@
         @endif
     </div>
     <div class="col-xs-8">
-        <h2>{{ $question.Title }}</h2>
-        <p>{{ $question.ContentLite }}</p>
-    </div>
-    <div class="col-xs-2">
-        @if($Author.hasProfile())
-            <!--Display Profile Photo-->
-        @else
-            <span class="fa-stack fa-lg">
-                <i class="fa fa-square fa-stack-x2"></i>
-                <i class="fa fa-user fa-stack-1x"></i>
-            </span>
-        @endif
+        <h2>{{ $question.title }}</h2>
+        <p>{{ $question.contentLite }}</p>
     </div>
     <div class="col-xs-12">
-        <label>{{ $question.Author.Name }}</label>
+        @include('view.profilelite', $author)
     </div>
 </div>
