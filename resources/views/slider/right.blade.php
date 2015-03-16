0786a5c9de5c3adb15500f74783d6eccee1e25de
Nicholai Goodall <Kranoscorp@gmail.com>
2015-03-16 15:47:47 -0400

    Added href for back button

--- resources/views/slider/right.blade.php
+++ resources/views/slider/right.blade.php
@@ -1,3 +1,5 @@
-<div class="col-xs-2 color-box">
-    <i class="fa fa-chevron-right"></i>
-</div>
+<a href={{ $previous }}>
+    <div class="col-xs-2 color-box slider-left">
+        <i class="fa fa-chevron-right"></i>
+    </div>
+</a>
