1d8a8eccda16347a9978955bf1ab2c81c8919c90
Nicholai Goodall <Kranoscorp@gmail.com>
2015-03-17 18:13:17 -0400

    Update profile.blade.php

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -1,30 +1,7 @@
 @extends('app')
 
 @section('content')
-<div class="row-fluid color-box profile">
-    @if($hasAvatar)
-        <!--Show Profile Pic-->
-    @else
-        <span class="fa-stack fa-lg">
-            <i class="fa fa-square-o fa-stack-2x"></i>
-            <i class="fa fa-user fa-stack-1x"></i>
-        </span>
-        @if(Auth::user() === $id)
-            <form class="form-horizontal" role="form" method="POST" action="{{ url('/user/'.$id.'/addAvatar') }}">
-                <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                
-                <label>Want to upload a photo?</label>
-                <div class="form-group">
-                    <input type="file" name="img">
-                </div>
-                <div class="form-group">
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </div>
-            </form>
-        @endif
-    @endif
-    {{ $userName }}
-</div>
+@include('view.profile.heavy', $author.user_id)
 @foreach($questions as $question)
     @include('view.questionlite', $question)
 @endforeach
