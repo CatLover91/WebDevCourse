0d84c2bd34aeda516b9f2272c776a3a12cb6a7f8
catlover91 <kranoscorp@gmail.com>
2015-03-30 07:17:07 +0000

    Switched form inputs to L4.2

--- app/views/page/home.blade.php
+++ app/views/page/home.blade.php
@@ -5,30 +5,12 @@
     <div class="col-xs-10">
         @if (Auth::check())
             <div class="row ask">
-                <form class="form-horizontal" role="form" method="POST" action="{{ url('/question/add') }}">
-                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
-
-                    <div class="form-group">
-                        <label class="span4 control-label">Title</label>
-                        <div class="span6">
-                            <input type="text" class="form-control" name="title">
-                        </div>
-                    </div>
-
-
-                    <div class="form-group">
-                        <label class="span4 control-label">Content</label>
-                        <div class="span6">
-                            <input type="textarea" class="form-control" name="content">
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <div class="span6 col-xs-offset-4">
-                            <button type="submit" class="btn btn-primary">Login</button>
-                        </div>
-                    </div>
-                </form>
+                {{ Form::open(array('url' => '/question/add')) }}
+                    {{ Form::token() }}
+                    {{ Form::text('title') }}
+                    {{ Form::text('content') }}
+                    {{ Form::submit('Login') }}
+                {{ Form::closr() }}
             </div>
         @endif
         @foreach ($questions as $question)
